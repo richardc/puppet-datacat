e9f760fc48ef93b7412639c52fe463e944c9da5a
Richard Clamp <richardc@unixbeard.net>
2013-07-05 15:47:41 +0100

    test that the error case both raises the correct error, and the correct message
    Also hack around that rspec-puppet's run matcher doesn't implement
    `#description` by switching back to explicitly naming examples.
    
    This can sit in a branch for a while.

--- spec/functions/template_body_spec.rb
+++ spec/functions/template_body_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe 'template_body' do
-  it { should run.with_params('template_body/really_should_never_exist.erb').and_raise_error(Puppet::ParseError) }
-  it { should run.with_params('template_body/test1.erb').and_return("Goodbye cruel world\n") }
+  describe "when called with a template that doesn't exist" do
+    it "should raise an error" do
+      should run.with_params('template_body/really_should_never_exist.erb').and_raise_error(Puppet::ParseError)
+    end
+
+    it "should say that the file is missing" do
+      should run.with_params('template_body/really_should_never_exist.erb').and_raise_error(/Could not find template/)
+    end
+  end
+
+  it "should return the body of a template that exists" do
+    should run.with_params('template_body/test1.erb').and_return("Goodbye cruel world\n")
+  end
 end
